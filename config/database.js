79468cdc029d2e891a46e4400fa14407cbc67f21
Nicolas Vuillermet <nicovuillermet@gmail.com>
2021-03-26 13:36:00 +0100

    Add dbhost in Pool configuration

--- config/database.js
+++ config/database.js
@@ -2,6 +2,7 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
+    host:process.env['DB_HOST'],
     port:process.env['DB_PORT'],
     database:process.env['DB_NAME'],
     user: process.env['DB_USER'],
